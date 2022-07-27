ccb0b55ff193b0aac65d3c908790767d22e51e4e
Merve Bacak <nmerveb@gmail.com>
2022-07-27 14:09:36 +0300

    Firebase config initialized.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: 'pet-health-care-67e01',
+    appId: '1:992901507213:web:2fbecfd3dbcbcdcd631ac4',
+    databaseURL: 'https://pet-health-care-67e01-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'pet-health-care-67e01.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDNaHz5fTT84arQqQcx01ymnS8l_I5VrdI',
+    authDomain: 'pet-health-care-67e01.firebaseapp.com',
+    messagingSenderId: '992901507213',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'pet-health-care-67e01',
+    appId: '1:992901507213:web:2fbecfd3dbcbcdcd631ac4',
+    databaseURL: 'https://pet-health-care-67e01-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'pet-health-care-67e01.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDNaHz5fTT84arQqQcx01ymnS8l_I5VrdI',
+    authDomain: 'pet-health-care-67e01.firebaseapp.com',
+    messagingSenderId: '992901507213',
+  },
 };
 
 /*
